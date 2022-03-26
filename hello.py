5aaa57f910a0123f364965959e1d883f60ca6c6e
Pranav <pr21_@pop-os.localdomain>
2022-03-26 13:46:40 +0530

    work

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from unicodedata import name
+from flask import Flask,render_template
 import datetime
+
+
 app = Flask(__name__)
-@app.route('/date')
+@app.route('/')
 def hello_world():
     toRet=datetime.datetime.now()
-    return str(toRet)
-    
+    name='pranav'
+    return render_template('index.html', name=name)
+
